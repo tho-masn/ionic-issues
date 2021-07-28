4629b22c8d37885f73faeb17abd949707088f931
Thomas Niedermaier <thomas.niedermaier@gmx.at>
2021-07-28 12:57:54 +0200

    Reproducable example of the issue where ion-modal presenting-element gets ignored

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,24 +5,40 @@
         <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+
+
+        <div
+          @click="onClickOpenModal"
+        >
+          Open modal
+        </div>
+
+        <ion-modal
+          :is-open="isOpen"
+          :presenting-element="presentingElement"
+          :swipe-to-close="true"
+          @did-dismiss="isOpen = false"
+        >
+          test
+        </ion-modal>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonModal } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -32,7 +48,22 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonModal
+  },
+
+  data () {
+    return {
+      isOpen: false,
+      presentingElement: null
+    }
+  },
+
+  methods: {
+    onClickOpenModal () {
+      this.presentingElement = document.getElementById('main-content')
+      this.isOpen = true
+    }
   }
 });
 </script>
@@ -40,7 +71,7 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +87,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
