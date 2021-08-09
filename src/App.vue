db2cc05df700a3e9835a4f82936b9079e441d5c9
Thomas Niedermaier <thomas.niedermaier@gmx.at>
2021-08-09 16:33:51 +0200

    Updated dependencies

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,11 @@
 
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'App',
+export default {
   components: {
     IonApp,
     IonRouterOutlet
   }
-});
+};
 </script>
